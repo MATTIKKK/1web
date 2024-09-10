23bda4e349d4db4754a07ea2861313c03fc0c5a1
Your Name <you@example.com>
2024-09-11 00:15:31 +0500

    header and footer for mobile

--- webix/src/App.tsx
+++ webix/src/App.tsx
@@ -1,12 +1,14 @@
 import "./app.css"
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/home-page/HomePage'
+import AboutUsPage from "./pages/about-us-page/AboutUs"
 
 const App = () => {
   return (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/about-us" element={<AboutUsPage />} />
 
         </Routes>
     </BrowserRouter>
--- webix/src/pages/about-us-page/AboutUs.tsx
+++ webix/src/pages/about-us-page/AboutUs.tsx
@@ -1,16 +1,16 @@
 import ClientForm from "../../components/client-form/ClientForm"
 import Footer from "../../components/footer/Footer"
 import Header from "../../components/header/Header"
-import "./home-page.css"
+import "./about-us.css"
 
-const HomePage = () => {
+const AboutUsPage = () => {
   return (
-    <div>
-      <Header />
-      <ClientForm />
-      <Footer />
+    <div className="about-us">
+        <Header />
+        <ClientForm />
+        <Footer />
     </div>
   )
 }
 
-export default HomePage
+export default AboutUsPage
